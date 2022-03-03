062240a304abea610c55ddaff320f94e7e709692
MarcoDiaz1 <drella_1991@hotmail.com>
2022-03-03 14:32:59 -0700

    Layot Test
    Signed-off-by: MarcoDiaz1 <drella_1991@hotmail.com>

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
+import { ThemeProvider } from "next-themes";
+import { ParallaxProvider } from "react-scroll-parallax";
+
+import Layout from "../layout/layout.index";
+import CustomMouse from "../components/customMouse";
+import MouseContextProvider from "../MouseContext";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ParallaxProvider>
+      <MouseContextProvider>
+        <CustomMouse />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </MouseContextProvider>
+    </ParallaxProvider>
+  );
 }
 
 export default MyApp;

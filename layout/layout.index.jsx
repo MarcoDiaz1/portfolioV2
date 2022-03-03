60b43c0e4f57a18a7030e7693fe7dba7161e7da2
MarcoDiaz1 <drella_1991@hotmail.com>
2022-03-03 13:45:33 -0700

    Layout test
    Signed-off-by: MarcoDiaz1 <drella_1991@hotmail.com>

--- layout/layout.index.jsx
+++ layout/layout.index.jsx
@@ -11,8 +11,6 @@ const Layout = ({ children }) => {
 
   return (
     <div className="flex flex-col md:flex-row md:cursor-none">
-      <Aside />
-
       <div className="w-full md:pl-[18vw] xl:pl-[15vw] md:w-[100vw] h-screen pt-[8vh] md:pt-0">
         {children}
       </div>

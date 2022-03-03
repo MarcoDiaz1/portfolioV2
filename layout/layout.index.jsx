6a68015e3c0d48379d4088c811419bebe93b8cb3
MarcoDiaz1 <drella_1991@hotmail.com>
2022-03-03 14:30:37 -0700

    Layout test
    Signed-off-by: MarcoDiaz1 <drella_1991@hotmail.com>

--- layout/layout.index.jsx
+++ layout/layout.index.jsx
@@ -1,6 +1,27 @@
+import { useRouter } from "next/router";
+
+import Aside from "../components/aside/aside.index";
+import SEO from "../SEO";
+import lang from "../lang/lang.index";
+import { paths } from "../data/data";
+
 const Layout = ({ children }) => {
+  const { locale, pathname } = useRouter();
+  const currentPath = paths.indexOf(pathname);
+
   return (
     <div className="flex flex-col md:flex-row md:cursor-none">
+      <SEO
+        title={
+          currentPath === 0 || currentPath === -1
+            ? "Eric Haller Portfolio"
+            : lang[locale].nav.content[currentPath]
+        }
+        url={pathname}
+        description={lang[locale].pages.desc[currentPath]}
+      />
+      <Aside />
+
       <div className="w-full md:pl-[18vw] xl:pl-[15vw] md:w-[100vw] h-screen pt-[8vh] md:pt-0">
         {children}
       </div>
--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,7 @@
-import { ThemeProvider } from "next-themes";
-import { ParallaxProvider } from "react-scroll-parallax";
-
-import Layout from "../layout/layout.index";
-import CustomMouse from "../components/customMouse";
-import MouseContextProvider from "../MouseContext";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <ThemeProvider attribute="class">
-      <ParallaxProvider>
-        <MouseContextProvider>
-          <CustomMouse />
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
-        </MouseContextProvider>
-      </ParallaxProvider>
-    </ThemeProvider>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
